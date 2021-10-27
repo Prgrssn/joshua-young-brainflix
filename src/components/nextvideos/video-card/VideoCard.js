20f4aa9c66a824897cf5a179a82545e554dd036a
Joshua Young <prgrssn@gmail.com>
2021-10-27 11:37:20 -0700

    added styling to the next up video list

--- src/components/nextvideos/video-card/VideoCard.js
+++ src/components/nextvideos/video-card/VideoCard.js
@@ -5,18 +5,17 @@ export default function VideoCard(props) {
   console.log(props);
   return (
     <article className="video-card">
-    <div className="video-card__img-container">
-      <img
-        className="video-card__img-container--img"
-        src={props.video.image}
-        alt={props.video.title}
-      ></img>
+      <div className="video-card__img-container">
+        <img
+          className="video-card__img-container--img"
+          src={props.video.image}
+          alt={props.video.title}
+        ></img>
       </div>
       <div className="video-card__details">
-        <h3 className="video-card__title">{props.video.title}</h3>
-        <h4 className="video-card__channel">{props.video.channel}</h4>
+        <p className="video-card__details--title">{props.video.title}</p>
+        <p className="video-card__details--channel">{props.video.channel}</p>
       </div>
-    </div>
     </article>
   );
 }
