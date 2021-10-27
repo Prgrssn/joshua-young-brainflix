22f7d75b2e7a731dd48603491f2673982b3ff2ed
Joshua Young <prgrssn@gmail.com>
2021-10-26 20:07:33 -0700

    created an avatar component and added to header

---
+++ src/components/avatar/Avatar.js
@@ -0,0 +1,15 @@
+import "./Avatar.scss";
+
+function Avatar() {
+  return (
+    <div>
+      <img
+        className="avatar"
+        src="../../assets/images/Mohan-muruge.jpg"
+        alt="Mohan Muruge"
+      />
+    </div>
+  );
+}
+
+export default Avatar;
---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import "../avatar/Avatar";
+import Avatar from "../avatar/Avatar";
+import "./Header.scss";
+
+function Header() {
+  return (
+    <div>
+      <h1>Hello World!</h1>
+      <Avatar />
+    </div>
+  );
+}
+
+export default Header;
