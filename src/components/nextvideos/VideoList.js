428692ddb208f14e2103e2c23bc63df2fa312d76
Joshua Young <prgrssn@gmail.com>
2021-10-28 08:37:11 -0700

    added border box sizing to the app.css

--- src/components/nextvideos/VideoList.js
+++ src/components/nextvideos/VideoList.js
@@ -4,10 +4,10 @@ import VideoCard from "./video-card/VideoCard";
 
 export default function VideoList(props) {
   return (
-    <article>
+    <section className="video-list-section">
       {props.video.map((video) => {
         return <VideoCard key={video.title} video={video} />;
       })}
-    </article>
+    </section>
   );
 }
