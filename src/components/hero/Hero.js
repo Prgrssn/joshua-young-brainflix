6e7af437ecd0177698ecac9b3d09a5280f9c8d8e
Joshua Young <prgrssn@gmail.com>
2021-10-28 23:00:58 -0700

    adjusted the size of the video in mobile view

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,16 +1,15 @@
 import React from "react";
 import CommentForm from "../comment-form/CommentForm";
 import CommentList from "../comment-list/CommentList";
-import VideoPlayer from "../video-player/VideoPlayer";
+// import VideoPlayer from "../video-player/VideoPlayer";
 import "./Hero.scss";
 
 export default function Hero(props) {
-  console.log(props);
   return (
     <section className="hero-section">
-      <VideoPlayer key={props.heroData} />;
+      {/* <VideoPlayer key={props.heroData} />;
       <CommentForm />
-      <CommentList comment={props.heroData} />
+      <CommentList comment={props.heroData} /> */}
     </section>
   );
 }
