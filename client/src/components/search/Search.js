1d7f5f5c63f58995a2feb300c66c542f3e1b74fb
Joshua Young <prgrssn@gmail.com>
2021-11-12 17:42:45 -0800

    untracking node modules

--- client/src/components/search/Search.js
+++ client/src/components/search/Search.js
@@ -1,6 +1,6 @@
 import Avatar from "../Avatar/Avatar";
 import { Link } from "react-router-dom";
-import "./Search.scss";
+import "./Bob.scss";
 import React from "react";
 
 export default function Search() {
